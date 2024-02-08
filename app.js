cda3b1d4ac1b453fb0845dbed760aaab62699ca9
ocin-effendy <nikoardia134@gmail.com>
2024-02-08 21:27:48 +0700

    feat: category(add & read db)

--- app.js
+++ app.js
@@ -4,8 +4,16 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// mongoose
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/db_staycation');
+
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+// admin router
+const adminRouter = require('./routes/admin');
 
 var app = express();
 
@@ -18,9 +26,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/sb-admin-2', express.static(path.join(__dirname, 'node_modules/startbootstrap-sb-admin-2')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+// admin
+app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
